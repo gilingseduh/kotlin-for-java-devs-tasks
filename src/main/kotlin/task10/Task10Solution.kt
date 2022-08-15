7bf82f9d6b78455b05c8c567b6a9c82b8a96f4f6
Rachmat Ibrahim <semarcapada@gmail.com>
2022-08-15 16:12:45 +0700

    Add Task10Solution.kt

---
+++ src/main/kotlin/task10/Task10Solution.kt
@@ -0,0 +1,15 @@
+package task10
+
+// Implement the property 'foo' so that it produced a different value on each access.
+// Note that you can modify the code outside the 'foo' getter (e.g. add additional imports or properties).
+
+var count = 0
+val foo: Int
+    get() = count++
+
+fun main(args: Array<String>) {
+    // The values should be different:
+    println(foo)
+    println(foo)
+    println(foo)
+}
